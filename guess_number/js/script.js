fad75e3f12ddaeb8ab897b2f0474fd5e086e7166
Mauricio Reynoso <maureynoso@csumb.edu>
2026-02-11 10:11:59 -0800

    Working through JS Intro Guess a Number Game. Finishing section 4 of 8

---
+++ guess_number/js/script.js
@@ -0,0 +1,9 @@
+//alert("running external JS code!")
+
+//Global variables
+/* floor rounds down to nearest integer
+(Math.random() * 99) +1 generates random int 1 through 99 */
+let randomNumber = Math.floor(Math.random() * 99) + 1;
+
+console.log(randomNumber);
+//document.querySelector("h1").style.color = "red";
