74df738cb048c48f36ab8abfc008990ee24d5bf7
Mauricio Reynoso <maureynoso@csumb.edu>
2026-02-11 11:33:44 -0800

    Finished step 5 of 8 of JS Intro Guess a Number Game Tutorial

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ https://mreynoso123.github.io/lab1_teamsite/ (Lab 1: Team Site)
 https://mreynoso123.github.io/hw1/ (HW1: HTML and CSS)
 
 https://mreynoso123.github.io/lab2/ (Lab 2: Guess the Number)
+
+https://mreynoso123.github.io/guess_number/ (JS Intro: Guess a Number Game Tutorial)
--- guess_number/js/script.js
+++ guess_number/js/script.js
@@ -4,6 +4,9 @@ let attempts = 0;
 
 initializeGame();
 
+//Event Listeners
+document.querySelector("#guessBtn").addEventListener("click", checkGuess);
+
 function initializeGame() {
     /* floor rounds down to nearest integer
     (Math.random() * 99) +1 generates random int 1 through 99 */
@@ -15,4 +18,18 @@ function initializeGame() {
 
     //adding focus to textbox
     document.querySelector("#playerGuess").focus();
+}
+
+function checkGuess() {
+    //placing feedback var here helps hide valid number range
+    let feedback = document.querySelector("#feedback");
+    feedback.textContent = "";
+    let guess = document.querySelector("#playerGuess").value;
+    console.log("Player guess: " + guess);
+
+    if (guess < 1 || guess > 99) {
+        feedback.textContent = "Enter a number between 1 and 99";
+        feedback.style.color = "red";
+        return;
+    }
 }
